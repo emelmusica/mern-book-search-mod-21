0a55352316ba00e9510e1b165547c3a50d726dc5
Em3L <martink_lopez@yahoo.com>
2023-10-17 18:58:48 +1100

    created files all over again cause idk what happened but none of my work was being saved now i am mad

---
+++ Develop/server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const typeDefs = require('./graphql/schema'); // Import your GraphQL schema
+const resolvers = require('./graphql/resolvers'); // Import your GraphQL resolvers
+const db = require('./config/connection');
+const routes = require('./routes');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Define your Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    // You can use this context to handle authentication
+    // Extract user information from request headers or cookies
+    const token = req.headers.authorization || '';
+    const user = getUserFromToken(token);
+    return { user };
+  },
+});
+
+// Apply the Apollo middleware to Express
+server.applyMiddleware({ app });
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
