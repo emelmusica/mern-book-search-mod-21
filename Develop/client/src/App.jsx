9ebf54233633b0c7e95a16643c0167933f29395f
Em3L <martink_lopez@yahoo.com>
2023-10-17 19:43:02 +1100

    updated app js code to meet criteria i think

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,32 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import './App.css';
 import Navbar from './components/Navbar';
+import { Home, Login, Signup, SavedBooks } from './pages'; // Import your route components
 
 function App() {
+  // Apollo Client instance
+  const client = new ApolloClient({
+    uri: 'https://your-app-name.herokuapp.com', // Replace with heroku Apollo Server's URL
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/saved" element={<SavedBooks />} />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
