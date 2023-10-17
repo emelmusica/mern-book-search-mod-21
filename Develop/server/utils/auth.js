74272037ddcf5ef42f73972f9bb9da5c3a75b8f3
Em3L <martink_lopez@yahoo.com>
2023-10-17 19:04:25 +1100

    added the updated auth js code

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,39 +1,34 @@
 const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // Function for our authenticated routes
+  authMiddleware: function (context) {
+    const authHeader = context.req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    if (authHeader) {
+      const token = authHeader.split('Bearer ')[1];
+
+      if (token) {
+        try {
+          const { data } = jwt.verify(token, secret, { maxAge: expiration });
+          return { user: data };
+        } catch (err) {
+          throw new AuthenticationError('Invalid or expired token');
+        }
+      }
+
+      throw new AuthenticationError('Authentication token must be in the format "Bearer <token>"');
     }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
+    throw new AuthenticationError('Authorization header must be provided');
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
